26aa20004a88c408e2d621d8ed39a1d496dd49e7
divonbriesen <divonbriesen@gmail.com>
2022-11-09 11:15:24 -0500

    d

---
+++ assignments/js-dom-lecture-activity/index.js
@@ -0,0 +1,21 @@
+const proveButtonClick = (event) => 
+{ 
+     
+    const inputText = event.target.parentNode.querySelector('input').value;
+    // create list item
+    const newListItem = document.createElement('li');
+    // create text item
+    const newTextItem = document.createTextNode(inputText);
+    // add text to list item
+    newListItem.appendChild(newTextItem);
+    // add list item to list
+    document.querySelector('ul').appendChild(newListItem);
+
+
+
+    //alert('you clicked the button to add:'  + inputText );
+}
+
+//document.querySelector("button").addEventListener('click', proveButtonClick);
+const addNewItemButton = document.querySelector("button");
+addNewItemButton.addEventListener('click', proveButtonClick); 
